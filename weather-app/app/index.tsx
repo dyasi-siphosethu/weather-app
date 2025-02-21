ddbb78eddff1dbc5d09cfe72b00069cd677112a3
Siphosethu Dyasi <siphosethudyasi@gmail.com>
2025-02-21 22:08:24 +0200

    second commit

--- weather-app/app/index.tsx
+++ weather-app/app/index.tsx
@@ -1,16 +1,61 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
 import '../global.css';
+import { StatusBar } from 'expo-status-bar';
+import { theme } from "@/theme/theme-style";
+
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { MapPinIcon } from 'react-native-heroicons/solid';
+import { useState } from "react";
 
 export default function Index() {
+  const [showSearch, toggleSearch] = useState(false);
+  const [locations, setLocations] = useState([1,2,3]);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-500">Edit app/index.tsx to edit this screen. screen</Text>
+    <View className="flex-1 relative">
+      <StatusBar style="light" />
+      <Image blurRadius={70} source={require('../assets/images/bg.png')} className="absolute h-full w-full" />
+      <SafeAreaView className="flex flex-1">
+        {/*search section*/}
+        <View style={{height: '7%'}} className="mx-4 relative z-50">
+          <View className="flex-row justify-end items-center rounded-full" style={{backgroundColor: showSearch ? theme.bgWhite(0.2): 'transparent'}}>
+            {
+              showSearch? (
+                <TextInput 
+                  placeholder="Search city"
+                  placeholderTextColor={'lightgray'}
+                  className="pl-6 h-10 pb-1 flex-1 text-base text-white"
+                />
+              ):null
+            }
+            <TouchableOpacity 
+              onPress={() => toggleSearch(!showSearch)}
+              style={{backgroundColor: theme.bgWhite(0.3)}}
+              className="rounded-full p-3 m-1"
+            >
+              <MagnifyingGlassIcon size="25" color="white"/>
+            </TouchableOpacity>
+          </View>
+          {
+            locations.length>0 && showSearch ? (
+              <View className="absolute w-full bg-gray-300 top-16 rounded-3xl">
+                {
+                  locations.map((loc, index)=>{
+                    let showBorder = index+1 != locations.length;
+                    return (
+                      <TouchableOpacity
+                        key={loc}
+                        className="flex-row items-center border-0 p-3 px-4 mb-1 border-b-2 border-b-gray-400">
+                          <MapPinIcon size="20" color="gray" />
+                          <Text>London, United Kingdom</Text>
+                      </TouchableOpacity>
+                    )
+                  })
+                }
+              </View>
+            ) : null
+          }
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
